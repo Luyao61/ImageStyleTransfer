9298ff255ae165382f5d4e4e15883bccdc3eec35
luyao61 <luyao912@gmail.com>
2016-11-13 23:49:30 -0500

    not much progress

--- styleTransfer.lua
+++ styleTransfer.lua
@@ -4,7 +4,24 @@ require 'image'
 require 'optim'
 require 'cutorch'
 require 'cunn'
-
 load_model = require 'load_model'
 
-load_model.loadVGG(128,128)
+
+model = load_model.loadVGG(128,128)
+
+
+params, gradParams = model:getParameters()
+
+local optimState = {learningRate = 0.01}
+
+function feval(params)
+   gradParams:zero()
+
+   local outputs = model:forward(batchInputs)
+   local loss = criterion:forward(outputs, batchLabels)
+   local dloss_doutputs = criterion:backward(outputs, batchLabels)
+   model:backward(batchInputs, dloss_doutputs)
+
+   return loss, gradParams
+end
+optim.sgd(feval, params, optimState)
