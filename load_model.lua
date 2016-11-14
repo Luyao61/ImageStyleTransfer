31750d179c859913949de0577b7c594287da26e0
luyao61 <luyao912@gmail.com>
2016-11-13 19:18:49 -0500

    normalize layer

--- load_model.lua
+++ load_model.lua
@@ -14,6 +14,7 @@ function module.loadVGG(height, width)
     local layer_type = torch.type(layer)
 
     if(layer_type == 'nn.SpatialMaxPooling') then
+      -- replace maxpooling layer to averagepooling
       local kW, kH = layer.kW, layer.kH
       local dW, dH = layer.dW, layer.dH
       local padW, padH = layer.padW, layer.padH
@@ -23,13 +24,22 @@ function module.loadVGG(height, width)
       model:insert(avg_pooling, i)
       print(("Replace Max Pool layer #%d with avg pool layer"):format(i))
     elseif (layer_type == 'nn.SpatialConvolution') then
-      local a =0
+      -- normalize the network
+      --[["scale the weights such that the mean activation of each convolutional
+          filter over images and positions equal to one"
+          paper "Image Style Transfer using CNN", page 2416]]
+      local filter_size = layer.weight:size(2)*layer.weight:size(3)*layer.weight:size(4)
+      for j = 1,layer.weight:size(1) do
+        local mean = layer.weight[1]:sum()/filter_size
+        layer.weight[1]:div(mean)
+      end
+      print(("Normalize layer: %s"):format(name))
     end
     --print(('%s; %s'):format(name,layer_type))
   end
   model:remove(46)
   model:remove(45)
-  model:remove(45)
+  model:remove(44)
   model:remove(43)
   model:remove(42)
   model:remove(41)
@@ -37,9 +47,7 @@ function module.loadVGG(height, width)
   model:remove(39)
   model:remove(38)
 
-
   model:cuda()
-  print(model)
   return model
 end
 
