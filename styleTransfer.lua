b2cb2bc843880ae9316eadb1de03802184165033
luyao61 <luyao912@gmail.com>
2016-11-18 14:01:32 -0500

    update 1118

--- styleTransfer.lua
+++ styleTransfer.lua
@@ -34,19 +34,22 @@ criterion = nn.MSECriterion()
 criterion:cuda()
 
 
-targets = model:get(20).output
+--targets = model:get(20).output
+targets = model:forward(content_img)
 for i=1,10 do
-  model:forward(input_image)
 
-  local output = model:get(20).output
+
+  local output = model:forward(input_image)
+--local output = model:get(20).output
 --[[  input_features_conv1_1 = model:get(1).output
   input_features_conv2_1 = model:get(6).output
   input_features_conv3_1 = model:get(11).output
   input_features_conv4_1 = model:get(20).output
   input_features_conv5_1 = model:get(29).output]]
   local err = criterion:forward(output, targets)
-  local df_do = criterion:backward(output, targets)
+  local gradLoss = criterion:backward(output, targets)
+  local gradInput = model:backward(input, gradLoss)
 
-  input_image = input_image + 0.1 * criterion.gradInput
+  input_image = input_image - 0.1 * gradInput
 
 end
