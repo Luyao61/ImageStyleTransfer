30a038c3b3357736b741f669774c8d63c3b83187
luyao61 <luyao912@gmail.com>
2016-11-23 21:30:58 -0500

    reconstruct image

--- load_model.lua
+++ load_model.lua
@@ -5,52 +5,50 @@ require 'loadcaffe'
 local module = {}
 
 function module.loadVGG(height, width)
-  vgg19 = loadcaffe.load('models/VGG_ILSVRC_19_layers_deploy.prototxt', 'models/VGG_ILSVRC_19_layers.caffemodel', 'nn')
-
-  local model = nn.sequential()
-  local content_layers = ['relu4_2']
-  local style_layers = ['relu1_1','relu2_1','relu3_1','relu4_1','relu5_1']
-  content_layer_index = 1
-  style_layer_index = 1
-  content_weight = 0.5
-  style_weight = 0.5
+  model = loadcaffe.load('models/VGG_ILSVRC_19_layers_deploy.prototxt', 'models/VGG_ILSVRC_19_layers.caffemodel', 'cudnn')
 
   local model_size = #model
   for i = 1, model_size do
-    if content_layer_index <= #content_layers or style_layer_index <= #style_layers then
-      local layer = model:get(i)
-      local name = layer.name
-      local layer_type = torch.type(layer)
+    local layer = model:get(i)
+    local name = layer.name
+    local layer_type = torch.type(layer)
 
-      if(layer_type == 'nn.SpatialMaxPooling') then
-        -- replace maxpooling layer to averagepooling
-        local kW, kH = layer.kW, layer.kH
-        local dW, dH = layer.dW, layer.dH
-        local padW, padH = layer.padW, layer.padH
+    if(layer_type == 'nn.SpatialMaxPooling') then
+      -- replace maxpooling layer to averagepooling
+      local kW, kH = layer.kW, layer.kH
+      local dW, dH = layer.dW, layer.dH
+      local padW, padH = layer.padW, layer.padH
 
-        local avg_pooling = nn.SpatialAveragePooling(kW, kH, dW, dH, padW, padH)
-        model:remove(i)
-        model:insert(avg_pooling, i)
-        print(("Replace Max Pool layer #%d with avg pool layer"):format(i))
-      else
-        model:add(layer)
-      end
-      if (name == content_layers[content_layer_index]) then
-        local target = net:forward(content_image):clone()
-        local loss_module = nn.ContentLoss(content_weight,target.false)
-        model:add(loss_module)
-        table.insert(content_losses, loss_module):float()
-        content_layer_index = content_layer_index + 1
-      end
-      if (name == style_layers[style_layer_index]) then
-        local gram = GramMatrix():float()
-
-
-        style_layer_index = style_layer_index + 1
-      end
+      local avg_pooling = nn.SpatialAveragePooling(kW, kH, dW, dH, padW, padH)
+      model:remove(i)
+      model:insert(avg_pooling, i)
+      print(("Replace Max Pool layer #%d with avg pool layer"):format(i))
+--    elseif (layer_type == 'nn.SpatialConvolution') then
+      -- normalize the network
+      --[["scale the weights such that the mean activation of each convolutional
+          filter over images and positions equal to one"
+          paper "Image Style Transfer using CNN", page 2416]]
+--      local filter_size = layer.weight:size(2)*layer.weight:size(3)*layer.weight:size(4)
+--      for j = 1,layer.weight:size(1) do
+--        local mean = layer.weight[j]:sum()/(filter_size)
+--        layer.weight[j]:div(mean)
+--        --print(layer.weight[j])
+--      end
+--      print(("Normalize layer: %s"):format(name))
     end
+    --print(('%s; %s'):format(name,layer_type))
   end
+  model:remove(46)
+  model:remove(45)
+  model:remove(44)
+  model:remove(43)
+  model:remove(42)
+  model:remove(41)
+  model:remove(40)
+  model:remove(39)
+  model:remove(38)
 
+  model:cuda()
   return model
 end
 
--- styleTransfer.lua
+++ styleTransfer.lua
@@ -6,52 +6,115 @@ require 'cutorch'
 require 'cunn'
 load_model = require 'load_model'
 
---load VGG 19 model;
---this model has 16 cov layers and 4 avg pool layer
---normalized weights
-model = load_model.loadVGG(128,128)
--- load content image
-content_img = image.load("examples/golden_gate.jpg")
-content_img = content_img:cuda()
---create an white noise input image that has the same size as content image
-input_image = torch.CudaTensor(content_img:size())
-input_image:uniform(0,1)
-image.display(input_image)
-image.display(content_img)
+local function main()
+  --load VGG 19 model;
+  --this model has 16 cov layers and 4 avg pool layer
+  --normalized weights
+  model = load_model.loadVGG(128,128)
 
---[[model:forward(content_img)
-print(model:get(1).name)
-content_features_conv1_1 = model:get(1).output
-print(model:get(6).name)
-content_features_conv2_1 = model:get(6).output
-print(model:get(11).name)
-content_features_conv3_1 = model:get(11).output
-print(model:get(20).name)
-content_features_conv4_1 = model:get(20).output
-print(model:get(29).name)
-content_features_conv5_1 = model:get(29).output]]
+  -- load content image
+  content_img = image.load("examples/golden_gate.jpg")
+  content_img = preprocess(content_img):float()
+  content_img = content_img:cuda()
 
-criterion = nn.MSECriterion()
-criterion:cuda()
-model:forward(content_img)
-targets = model:get(20).output
+  --create an white noise input image that has the same size as content image
+  input_image = torch.DoubleTensor(content_img:size())
+  input_image:uniform(0,1)
+  input_image = preprocess(input_image):float()
+  input_image = input_image:cuda()
+  image.display(input_image)
 
-print(#targets)
-print(targets[100][5])
-for i=1,1 do
+  content_features_conv4_1 = model:forward(content_img):clone()
+--[[  print(model:get(1).name)
+  content_features_conv1_1 = model:get(2).output:clone()
+  print(model:get(6).name)
+  content_features_conv2_1 = model:get(7).output:clone()
+  print(model:get(11).name)
+  content_features_conv3_1 = model:get(12).output:clone()
+  print(model:get(20).name)]]
+  --content_features_conv4_1 = model:get(21).output:clone()
+--[[  print(model:get(29).name)
+  content_features_conv5_1 = model:get(30).output:clone()
+]]
+  criterion = nn.MSECriterion()
+  criterion:cuda()
+  local _, gradParams = model:getParameters()
+  local function feval(x)
+    gradParams:zero()
+
+    -- Just run the network back and forth
+    local yhat = model:forward(x)
+    --local yhat = model:get(21).output:clone()
+    local loss = criterion:forward(yhat,content_features_conv4_1)
+    local gradLoss = criterion:backward(yhat,content_features_conv4_1)
+    local gradInput = model:backward(x,gradLoss)
+
+    collectgarbage()
+    return loss, gradInput:view(gradInput:nElement())
+  end
+
+  optim_state = {
+    learningRate = 0.03
+    }
+  for t = 1, 100 do
+  	local x, losses = optim.adam(feval, input_image, optim_state)
+  	print('Iteration number: '.. t ..'; Current loss: '.. losses[1])
+  end
+
+  output_image = deprocess(input_image:clone():double())
+  image.display(output_image)
+end
+
+function preprocess(img)
+  local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
+  local perm = torch.LongTensor{3, 2, 1}
+  img = img:index(1, perm):mul(256.0)
+  mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
+  img:add(-1, mean_pixel)
+  return img
+end
+function deprocess(img)
+  local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
+  mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
+  img = img + mean_pixel
+  local perm = torch.LongTensor{3, 2, 1}
+  img = img:index(1, perm):div(256.0)
+  return img
+end
+main()
+--[[function GramMatrix()
+  local net = nn.Sequential()
+  net:add(nn.View(-1):setNumInputDims(2))
+  local concat = nn.ConcatTable()
+  concat:add(nn.Identity())
+  concat:add(nn.Identity())
+  net:add(concat)
+  net:add(nn.MM(false, true))
+  return net
+end]]
+
+
+
+
+
+
+
+
+
+--[[
+
+for i=1,2 do
   model:forward(input_image)
-  output = model:get(20).output
+  output = model:get(20).output:clone()
   --local output = model:get(20).output
-  print(torch.all(torch.eq(targets, output)))
 
---[[  input_features_conv1_1 = model:get(1).output
-  input_features_conv2_1 = model:get(6).output
-  input_features_conv3_1 = model:get(11).output
-  input_features_conv4_1 = model:get(20).output
-  input_features_conv5_1 = model:get(29).output]]
+
   local err = criterion:forward(output, targets)
+  print(err)
+
   local gradLoss = criterion:backward(output, targets)
   local gradInput = model:backward(input_image, gradLoss)
-  print(err)
   input_image = input_image - 0.1 * gradInput
 end
+image.display(input_image)
+]]
